0c78dc4dddc7ed427d73954dd17b38575e4d048a
Julia Ramos <juliaramosguedes@gmail.com>
2021-04-30 23:53:21 -0300

    fix build error

--- src/components/card.tsx
+++ src/components/card.tsx
@@ -1,10 +1,15 @@
-import React from 'react';
+import React, { ReactNode } from 'react';
 
 import { Card as CardStyle } from '../styles/components/Card';
 import { CheckCircle } from '@styled-icons/heroicons-outline/CheckCircle';
 import theme from '../styles/theme';
 
-const Card: React.FC = ({ alignCenter, children }) => {
+interface CardProps {
+    alignCenter?: boolean;
+    children: ReactNode;
+}
+
+const Card: React.FC<CardProps> = ({ alignCenter, children }) => {
     return (
         <CardStyle alignCenter={alignCenter}>
             <CheckCircle size="30px" color={theme.colors.primary} />
